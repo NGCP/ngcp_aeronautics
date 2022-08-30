dd3e7065434c808551c35d460418e67a05be7c9c
Volodymyr Mashchyts <vmashchyts@gmail.com>
2022-08-30 12:18:59 -0700

    Initial commit of stati_prop_optimizer
    Added static prop finder by Santiago Robles (santiagor00)

---
+++ static_prop_optimizer/objects.py
@@ -0,0 +1,8 @@
+class Propeller:
+   def __init__(self):
+      self.name = None
+      self.thrust_list = None
+      self.power_list = None
+      self.torque_list = None
+      self.cp_list = None
+      self.ct_list = None
